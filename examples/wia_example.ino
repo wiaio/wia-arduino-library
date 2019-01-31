7fe02c3268eaffacde2da7861ad7753fcc242cc7
alan <alan.donoghue@wia.io>
2019-01-31 13:47:32 +0000

    Intial commit for Library

---
+++ examples/wia_example.ino
@@ -0,0 +1,34 @@
+#include "wia.h"
+#include <Arduino.h>
+#include <WiFi.h>
+
+const char* ssid     = ""; // Your WiFi ssid
+const char* password = ""; //Your Wifi password
+String access_token = ""; // Your device secret key
+
+Wia wia = Wia(access_token);
+
+void setup() {
+   Serial.begin(115200);
+   while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+   }
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+
+}
+
+void loop() {
+    wia.create_event("hello-wia", "");
+    delay(5000);
+}
